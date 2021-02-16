5923694da55cba9dd5aa936cf003c2bcf148455d
Johnny Austen <65448382+jmaletto1@users.noreply.github.com>
2021-02-16 18:27:54 +0000

    Code Comments

--- js/app.js
+++ js/app.js
@@ -8,6 +8,13 @@ searchBar.addEventListener("keyup", () => {
   let input = searchBar.value.toLowerCase();
   console.log(input);
 
+  /*
+    Here, we loop through the data to check if the innerHTML, which has been
+    converted to lowercase, contains the same input as the search bar. If it
+    doesn't, said (non-matching) items are hidden. If there is a match (or 
+    if the search term is removed), the display is set to "initial".
+  */
+
   for (x = 0; x < data.length; x++) {
     if (!data[x].innerHTML.toLowerCase().includes(input)) {
       data[x].style.display = "none";
