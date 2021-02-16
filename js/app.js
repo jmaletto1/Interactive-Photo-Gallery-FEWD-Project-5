ff3b8895c04076562f564dab1b99c37425c779a8
Johnny Austen <65448382+jmaletto1@users.noreply.github.com>
2021-02-16 18:25:32 +0000

    Added Javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-baguetteBox.run(".gallery");
+// Global Variables
+const searchBar = document.getElementById("search");
+const searchValue = searchBar.value;
+const data = document.getElementsByClassName("photo");
+
+function photoSearch() {
+  console.log(searchValue);
+}
+
+searchBar.addEventListener("keyup", () => {
+  let input = searchBar.value.toLowerCase();
+  console.log(input);
+
+  for (x = 0; x < data.length; x++) {
+    if (!data[x].innerHTML.toLowerCase().includes(input)) {
+      data[x].style.display = "none";
+    } else {
+      data[x].style.display = "initial";
+    }
+  }
+});
